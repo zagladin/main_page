093aeac30aaed6f4821f53d3bb49ce0dc6e73932
George Zagladin <cygnus-x-1@mail.ru>
2021-02-11 23:30:58 +0300

    Some actions

---
+++ script.js
@@ -0,0 +1,27 @@
+const errorStatus = document.getElementsByClassName("error")[0];
+const errorMessage = document.getElementsByClassName("message")[0];
+const contacts = document.getElementsByClassName('contact')[0];
+
+const errorStatusText = "700 No Content Yet";
+const errorMessageText = "No available information";
+
+window.onload = function runningString() {
+  filler(errorStatus, errorStatusText, 200);
+  setTimeout(() => {
+    filler(errorMessage, errorMessageText, 150);
+  }, 3600);
+
+  setTimeout(() => {
+      contacts.style.animation='show 1s linear';
+      contacts.style.opacity=1;
+  }, 7200)
+};
+
+function filler(element, text, delay) {
+  const textToArray = text.split("");
+  textToArray.forEach((letter, i) => {
+      setTimeout(() => {
+        element.innerHTML += letter;
+      }, i * delay)
+  });
+};
