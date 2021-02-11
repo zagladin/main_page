e0659141bcf815f8e186ff8829afdc343524d3bb
George Zagladin <cygnus-x-1@mail.ru>
2021-02-12 00:17:15 +0300

    ESlint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const errorStatus = document.getElementsByClassName("error")[0];
 const errorMessage = document.getElementsByClassName("message")[0];
-const contacts = document.getElementsByClassName('contact')[0];
+const contacts = document.getElementsByClassName("contact")[0];
 
 const errorStatusText = "700 No Content Yet";
 const errorMessageText = "No available information";
@@ -12,16 +12,16 @@ window.onload = function runningString() {
   }, 3600);
 
   setTimeout(() => {
-      contacts.style.animation='show 1s linear';
-      contacts.style.opacity=1;
-  }, 7200)
+    contacts.style.animation = "show 1s linear";
+    contacts.style.opacity = 1;
+  }, 7200);
 };
 
 function filler(element, text, delay) {
   const textToArray = text.split("");
   textToArray.forEach((letter, i) => {
-      setTimeout(() => {
-        element.innerHTML += letter;
-      }, i * delay)
+    setTimeout(() => {
+      element.innerHTML += letter;
+    }, i * delay);
   });
-};
+}
