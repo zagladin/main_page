f92c16484047be438acbe58f6ef16ab3838ca363
1 <imcygnus.1@gmail.com>
2022-03-20 16:02:18 +0300

    Styles rework; new color scheme

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const { jsPDF } = window.jspdf;
-
 const olderExperience = document.getElementById('older');
-const elementToPrint = document.getElementById('to-print');
 
 function scrollToElement() {
   const scrollY = olderExperience.offsetTop;
@@ -12,37 +9,16 @@ function scrollToElement() {
   });
 }
 
-function saveToPDF() {
-  window.html2canvas = html2canvas;
-    const doc = new jsPDF('p', 'pt', 'a4');
+function printCV() {
+  window.print();
+}
 
-    doc.html(elementToPrint, {
-      html2canvas: {
-        width: 230,
-        scale: 0.56,
-      },
-      callback: function(pdf) {
-        pdf.save("Zagladin-CV.pdf");
-      }
-    });
-  // const worker = window.html2pdf();
-  // const params = {
-  //   margin:       0,
-  //   filename:     'Zagladin-CV.pdf',
-  //   image:        { type: 'jpeg', quality: 1 },
-  //   html2canvas:  { scale: 2 },
-  //   pagebreak: { mode: ['avoid-all', 'css', 'legacy'], after: ['.contacts'] },
-  //   jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-  // };
-  //
-  // worker
-  // .set(params)
-  // .from(elementToPrint)
-  // .toContainer()
-  // .toCanvas()
-  // .toImg()
-  // .toPdf()
-  // .save()
+function up() {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 }
 
 function delayBeforeTheScroll() {
