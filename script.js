54e63e723a06b0f738fbc7522a9260cfe6a17873
1 <imcygnus.1@gmail.com>
2022-03-20 14:17:30 +0300

    PDF extraction

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { jsPDF } = window.jspdf;
+
 const olderExperience = document.getElementById('older');
 const elementToPrint = document.getElementById('to-print');
 
@@ -11,8 +13,36 @@ function scrollToElement() {
 }
 
 function saveToPDF() {
-  console.log('SAVE TO PDF');
-  window.html2pdf(elementToPrint);
+  window.html2canvas = html2canvas;
+    const doc = new jsPDF('p', 'pt', 'a4');
+
+    doc.html(elementToPrint, {
+      html2canvas: {
+        width: 230,
+        scale: 0.56,
+      },
+      callback: function(pdf) {
+        pdf.save("Zagladin-CV.pdf");
+      }
+    });
+  // const worker = window.html2pdf();
+  // const params = {
+  //   margin:       0,
+  //   filename:     'Zagladin-CV.pdf',
+  //   image:        { type: 'jpeg', quality: 1 },
+  //   html2canvas:  { scale: 2 },
+  //   pagebreak: { mode: ['avoid-all', 'css', 'legacy'], after: ['.contacts'] },
+  //   jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+  // };
+  //
+  // worker
+  // .set(params)
+  // .from(elementToPrint)
+  // .toContainer()
+  // .toCanvas()
+  // .toImg()
+  // .toPdf()
+  // .save()
 }
 
 function delayBeforeTheScroll() {
